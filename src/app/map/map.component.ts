bed2e9084b7ecf521f96c4da63d2695770383950
mohammad ayesh <110039905+mohammad-ayeshh@users.noreply.github.com>
2023-01-15 14:56:42 +0300

    Update map.component.ts

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -14,7 +14,7 @@ export class MapComponent implements OnInit  {
   map!: mapboxgl.Map;
   city!:string;
 
-  accessToken:string="pk.eyJ1IjoibW9oYW1tYWQtYXllc2giLCJhIjoiY2xjZWx4eHo4NzhwbjNwa2VnaXBsOWlweiJ9.SiTucTeZYrzSa8EpGu-h7A"
+  accessToken:string="YOUR Access Token"
    results: any;
 constructor(private http: HttpClient){
   const path = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
